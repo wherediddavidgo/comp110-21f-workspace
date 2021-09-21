b69dda8d3f646646cdd0c2d6d4022fba7635bbab
David Go <dego@email.unc.edu>
2021-09-21 11:39:24 -0400

    Completed PJ00

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,12 +1,28 @@
 __name__: str = "__main__"
-score: int = 0
+points: int = 0
+NAMED_CONSTANT: str = "\U00000000"
 def main() -> None:
+    global points
     greet()
     game_loop: int = int(input("What would you like to do? 1 to play, 2 to quit "))
-    if game_loop == 1:
-        print(f"You have {score} points.")
-        choice = int(input(questionGenerator()))
-        scorer(choice)
+    i: int
+    while game_loop == 1:
+        i = 0
+        
+        while i < 3:
+            questionGenerator()
+            choice = int(input())
+            scorer(choice)
+            if points == 1:
+                print(f"You have {points} point.")
+            else:
+                print(f"You have {points} points.")
+            i += 1
+        game_loop = int(input("You've played three rounds. Would you like to keep going? 1 to play, 2 to quit "))
+        while game_loop != 1 and game_loop != 2:
+            game_loop = int(input("That's not a valid choice. Please select again. 1 to play, 2 to quit "))
+    #quit
+        
     
 
 
@@ -18,39 +34,35 @@ choice: int
 
 
 def scorer(choice: int) -> int:
+    global points
     if choice == 2:
-        score += 1
-        print(f"{player}, you've earned a point for being more like David! Good job")
+        points += 1
+        print(f"{player}, you've earned a point for being more like David! Good job \U0001F973")
     elif choice == 1:
-        score -= 1
-        print(f"{player}, you've lost a point for being more like Adin! Better luck next time")
+        points -= 1
+        print(f"{player}, you've lost a point for being more like Adin! Better luck next time \U0001F62D")
     else:
-        print("That's not a valid choice. Try again.")
-    return(score)
+        print("That's not a valid choice. Try again. \U0001F615")
+    return(points)
 
-def questionGenerator() -> str:
+def questionGenerator() -> None:
     from random import randint
     question: str = ""
-    rng: int = randint(1, 5)
+    rng: int = randint(1, 7)
     if rng == 1:
-        question = "Are you allergic to nuts? 1 for Yes, 2 for No "
+        print("Are you allergic to nuts? \U0001F95C 1 for Yes, 2 for No")
     elif rng == 2:
-        question = "Do you have curly hair? 1 for Yes, 2 for No "
+        print("Do you have curly hair? 1 for Yes, 2 for No")
     elif rng == 3:
-        question = "Is your height >= 5 feet 9 inches? 1 for Yes, 2 for No "
+        print("Is your height >= 5 feet 9 inches? \U00002B06\U00002B07 1 for Yes, 2 for No")
     elif rng == 4:
-        question = "What's your major? 1 for computer science, 2 for geology "
+        print(f"What's your major? 1 for computer science \U0001F4BB, 2 for geology \U000026F0")
     elif rng == 5:
-        question = "What sports do you like? 1 for hockey, 2 for track and field "
-    return(question)
-"""
-def gameControl() -> None:
-    global choice
-    choice = int(input("Enter your choice: "))
-    print("What would you like to do? 1 to play, 2 to quit")
-    if choice == 1:
-        print(questionGenerator)
-        scorer
-"""
+        print("What is your favorite sport? 1 for hockey \U0001F3D2, 2 for track and field \U0001F3BD")
+    elif rng == 6:
+        print("Is your dorm decorated nicely? 1 for yes, 2 for no")
+    elif rng == 7:
+        print("Which mRNA vaccine did you get? \U0001F489 1 for Moderna, 2 for Pfizer")
+
 if __name__ == "__main__":
     main()
